9a35ff763e10698873b748696e0b2ec13e19a83d
Dean-Q <Dean_xiu@outlook.com>
2024-03-20 17:42:09 +0800

    min stack

---
+++ 88_questions/java/queue_and_stack/MinStack.java
@@ -0,0 +1,43 @@
+package queue_and_stack;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * Question link
+ *
+ * @see <a href="https://leetcode.cn/problems/min-stack/description/">min-stack</a>
+ */
+public class MinStack {
+    Deque<Integer> mainStack;
+    //assistant stack to store min value
+    Deque<Integer> minStack;
+    public MinStack() {
+        mainStack = new LinkedList<>();
+        minStack = new LinkedList<>();
+        minStack.push(Integer.MAX_VALUE);
+    }
+
+    /**
+     * for all operations
+     * TC: O(1)
+     * SC: O(n)  n is the capacity of stack
+     */
+    public void push(int val) {
+        mainStack.push(val);
+        minStack.push(Math.min(mainStack.peek(),val));
+    }
+
+    public void pop() {
+        mainStack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return mainStack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
