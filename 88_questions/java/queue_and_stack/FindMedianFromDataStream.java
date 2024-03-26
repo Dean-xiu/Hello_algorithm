b481f0fed6efeb24544d4c96837f54c15e78f8e4
Dean-xiu <alien_B612@163.com>
2024-03-26 08:34:26 +0800

    find median from data stream

---
+++ 88_questions/java/queue_and_stack/FindMedianFromDataStream.java
@@ -0,0 +1,24 @@
+package queue_and_stack;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Question link
+ *
+ * @see <a href="https://leetcode.cn/problems/find-median-from-data-stream/description/">find-median-from-data-stream</a>
+ */
+public class FindMedianFromDataStream {
+    public FindMedianFromDataStream() {
+
+    }
+
+    public void addNum(int num) {
+
+    }
+
+    public double findMedian() {
+    }
+}
