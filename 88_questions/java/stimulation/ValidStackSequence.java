a32564a4257912e25f799f1bbbaf447f9739299f
Dean-xiu <alien_B612@163.com>
2024-04-16 23:15:39 +0800

    valid stack sequence

---
+++ 88_questions/java/stimulation/ValidStackSequence.java
@@ -0,0 +1,32 @@
+package stimulation;
+
+import java.util.Stack;
+
+/**
+ * Question link
+ *
+ * @see <a href=""></a>
+ */
+public class ValidStackSequence {
+    /**
+     * stimulate
+     * TC: O(n)  n is the capacity of pushed/popped
+     * SC: O(n)  n is the capacity of assist stack
+     * @param pushed
+     * @param popped
+     * @return
+     */
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> assist = new Stack<>();
+        int i=0;
+
+        for (int num : pushed) {
+            assist.push(num);
+            while (!assist.isEmpty() && assist.peek() == popped[i]) {
+                assist.pop();
+                i++;
+            }
+        }
+        return assist.isEmpty();
+    }
+}
