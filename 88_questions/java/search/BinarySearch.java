1b5077e1cc7763a3f12ccd563a4042b0bd0172e1
Dean-xiu <alien_B612@163.com>
2024-04-20 17:33:21 +0800

    binary search

---
+++ 88_questions/java/search/BinarySearch.java
@@ -0,0 +1,32 @@
+package search;
+
+/**
+ * Question link
+ *
+ * @see <a href="https://leetcode.cn/problems/binary-search/description/">binary-search</a>
+ */
+public class BinarySearch {
+    /**
+     * Binary search
+     * TC: O(logn)  n is the length of nums
+     * SC: O(1)
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int search(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+
+        while (l <= r) {
+            int mid = (r - l) / 2 + l;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                r = mid - 1;
+            } else if (nums[mid] < target) {
+                l = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
